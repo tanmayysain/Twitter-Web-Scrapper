6e1556e0200d5c5b21d21e37ecf84234a51a401a
tanmayysain <tanmaysain02@gmail.com>
2024-06-07 01:45:39 +0530

    Revert "Implemented the Selenium and Storing trends as an Object in an array with unique ID and timestamp;  Next Step: Adding Ip through Proxy"
    This reverts commit 43b648c30bb495bf1347af64ff5c73c1acc99fc6.

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { login } = require('../Controllers/Scrape');
-const router = express.Router();
-
-router.get('/scrape', login);
-  
-module.exports = router;
